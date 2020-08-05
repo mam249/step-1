9e7f7664e10e59161d27f70eed7b5698b5320c57
Azhara <azhara.assanova@gmail.com>
2020-08-05 12:05:11 +0000

    Implement service worker and add manifest for the website

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -203,3 +203,15 @@ function getSentiment(score) {
     return "😤";
   }
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
---
+++ portfolio/src/main/webapp/sw.js
@@ -0,0 +1,67 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/no-connection.html'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Inside the install handler:', event);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Inside the fetch handler:', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Inside the activate handler:', event);
+  var cacheAllowlist = [];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
